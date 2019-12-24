85d9959ae85f1b80519fda87e213d653acd1c388
jayruin <28711665+jayruin@users.noreply.github.com>
2019-12-23 19:52:45 -0500

    Add project files.

---
+++ AppLauncher/AppViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace AppLauncher
+{
+    public class AppViewModel
+    {
+        public SortedDictionary<string, string> AppsDictionary { get; set; }
+
+        public List<string> AppNamesCollection
+        {
+            get
+            {
+                return new List<string>(AppsDictionary.Keys);
+            }
+        }
+
+        public AppViewModel(IDictionary<string, string> dictionary)
+        {
+            AppsDictionary = new SortedDictionary<string, string>(dictionary);
+        }
+    }
+}
---
+++ AppLauncher/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Diagnostics;
+using System.Text.Json;
+using System.IO;
+
+namespace AppLauncher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public AppViewModel viewModel { get; set; }
+        private readonly string jsonFileName;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            jsonFileName = "json.json";
+            try
+            {
+                viewModel = new AppViewModel(JsonSerializer.Deserialize<Dictionary<string, string>>(File.ReadAllText(jsonFileName)));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                viewModel = new AppViewModel(new Dictionary<string, string>());
+            }
+            DataContext = viewModel;
+        }
+
+        private void buttonLaunch_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Process.Start(viewModel.AppsDictionary[comboBoxApps.SelectedItem.ToString()]);
+                Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
